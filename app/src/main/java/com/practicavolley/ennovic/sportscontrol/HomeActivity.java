b30d5aa04d3e945ada9b8ab2fe55233fc73ac955
Ivonne Barco <ivonn1022@gmail.com>
2018-07-15 03:11:47 -0500

    Pasando variables (nombre y rol) al Dashboard

--- app/src/main/java/com/practicavolley/ennovic/sportscontrol/HomeActivity.java
+++ app/src/main/java/com/practicavolley/ennovic/sportscontrol/HomeActivity.java
@@ -4,12 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.practicavolley.ennovic.sportscontrol.Modelos.Usuario;
 
 public class HomeActivity extends AppCompatActivity {
 
     private LinearLayout athletes, group, training, sports, plans, setting, time;
+    TextView nombreUsuario, rolUsuario;
+    private Usuario user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +37,12 @@ public class HomeActivity extends AppCompatActivity {
                 //startActivity(intent);
             }
         });
+
+        Bundle bundle = getIntent().getExtras();
+        user = bundle.getParcelable("DATOS_USER");
+        user.getId();
+        ((TextView) findViewById(R.id.nom_usuario_home)).setText("¡HOLA "+user.getNombre().toUpperCase()+"!");
+        user.getUsername();
+        ((TextView) findViewById(R.id.role_usuario_home)).setText(user.getRole().toUpperCase());
     }
 }
