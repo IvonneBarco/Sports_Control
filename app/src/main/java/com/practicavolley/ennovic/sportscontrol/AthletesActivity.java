508754b83a9cb67442731cbd8236adfce6c3e1d9
Ivonne Barco <ivonn1022@gmail.com>
2018-07-16 00:01:53 -0500

    nuevas vistas e items

---
+++ app/src/main/java/com/practicavolley/ennovic/sportscontrol/AthletesActivity.java
@@ -0,0 +1,13 @@
+package com.practicavolley.ennovic.sportscontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AthletesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_athletes);
+    }
+}
